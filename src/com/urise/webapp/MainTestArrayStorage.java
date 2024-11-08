b80812d29cc76fe0d0203ff33eb2c72feb7dd1c0
tttenko <tttenko37@gmail.com>
2024-11-08 09:01:12 +0300

    Выполненное домашнее задание

---
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -0,0 +1,47 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.ArrayStorage;
+
+public class MainTestArrayStorage {
+    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+
+    public static void main(String[] args) {
+        Resume r1 = new Resume();
+        r1.setUuid("uuid1");
+        Resume r2 = new Resume();
+        r2.setUuid("uuid2");
+        Resume r3 = new Resume();
+        r3.setUuid("uuid3");
+        Resume r4 = new Resume();
+        r4.setUuid("uuid4");
+
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r4);
+
+
+        System.out.println("\nGet r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+
+        ARRAY_STORAGE.update(r3);
+
+        printAll();
+        ARRAY_STORAGE.delete(r3.getUuid());
+        printAll();
+        ARRAY_STORAGE.clear();
+        printAll();
+
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+    }
+
+    static void printAll() {
+        System.out.println("\nGet All");
+        for (Resume r : ARRAY_STORAGE.getAll()) {
+            System.out.println(r);
+        }
+    }
+}
