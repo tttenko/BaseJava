0608d9931669ce85f738787e1965bbaf764c7116
Maksim Koptenko <tttenko37@gmail.com>
2024-11-14 10:07:32 +0300

    add tests

--- src/com/urise/webapp/storage/MainReflection.java
+++
@@ -1,9 +0,0 @@
-package com.urise.webapp.storage;
-
-import com.urise.webapp.model.Resume;
-
-public class MainReflection {
-    public static void main(String[] args) {
-
-    }
-}
--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AbstractArrayStorageTest {
+abstract class AbstractArrayStorageTest {
 
     protected Storage storage;
 
