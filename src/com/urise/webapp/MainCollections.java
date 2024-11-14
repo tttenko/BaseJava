4a5624c8f053ae11d12259d779da6292118bd73c
Maksim Koptenko <tttenko37@gmail.com>
2024-11-14 18:05:10 +0300

    коммит к 5 уроку

---
+++ src/com/urise/webapp/MainCollections.java
@@ -0,0 +1,33 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Objects;
+
+public class MainCollections {
+
+    private static final Resume RESUME_1 = new Resume("uuid1");
+    private static final Resume RESUME_2 = new Resume("uuid2");
+    private static final Resume RESUME_3 = new Resume("uuid3");
+
+    public static void main(String[] args) {
+        Collection<Resume> collection = new ArrayList<>();
+
+        collection.add(RESUME_1);
+        collection.add(RESUME_2);
+        collection.add(RESUME_3);
+
+        Iterator<Resume> iterator = collection.iterator();
+
+        while (iterator.hasNext()) {
+            Resume r = iterator.next();
+            if (Objects.equals(r.getUuid(), "uuid1")) {
+                iterator.remove();
+            }
+        }
+        System.out.println(collection.toString());
+    }
+}
