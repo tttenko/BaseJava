112d43dea9ce1058fde7a2e991a5f47fe26565b2
tttenko <tttenko37@gmail.com>
2024-12-06 19:47:11 +0300

    hw9 рекурсивный вывод с отступами

--- src/com/urise/webapp/util/MainFile.java
+++ src/com/urise/webapp/util/MainFile.java
@@ -10,17 +10,17 @@ public class MainFile {
     public static void main(String[] args) {
         Path path = Path.of("C:\\Users\\tttenko\\IdeaProjects\\BaseJava\\BaseJava\\src");
 
-        printAllFiles(path);
+        printAllFiles(path, "");
     }
 
-    public static void printAllFiles(Path dir) {
+    public static void printAllFiles(Path dir, String indent) {
        try (DirectoryStream<Path> list = Files.newDirectoryStream(dir)) {
             for (Path path : list) {
                 if (Files.isRegularFile(path)) {
-                    System.out.println("File: " + path.getFileName());
+                    System.out.println(indent + "File: " + path.getFileName());
                 } else if (Files.isDirectory(path)) {
-                    System.out.println("Directory: " + path.getFileName());
-                    printAllFiles(path);
+                    System.out.println(indent + " Directory: " + path.getFileName());
+                    printAllFiles(path, indent +  "  ");
                 }
             }
         } catch (IOException e) {
