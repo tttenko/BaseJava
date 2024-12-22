5f1656c53a9dbc4c83e4a057846d0905a3c22546
tttenko <tttenko37@gmail.com>
2024-12-22 16:46:30 +0300

    hw13 create database

---
+++ config/init_db.sql
@@ -0,0 +1,33 @@
+create table resume
+(
+    uuid
+        char(36)
+        not null
+        constraint resume_pk
+        primary key,
+    full_name
+        text not null
+);
+
+create table contact
+(
+    id
+        integer
+        not null
+        constraint contact_pk
+        primary key,
+    resume_uuid
+        char(36)
+        not null
+        references resume (uuid)
+        on delete cascade,
+    type
+        text
+        not null,
+    value
+        text
+        not null
+
+);
+
+create unique index contact_uuid_type_index on contact (resume_uuid, type);
